60eeffbbbd3d7cd66962cc840e113ee69fc87d70
Fauzan Husain <achmadfauzan1340@gmail.com>
2025-03-02 14:49:41 +0800

    more responsive on 75% zoom out /about

--- pages/about.js
+++ pages/about.js
@@ -7,14 +7,14 @@ const About = () => {
             <div className="relative w-[90%] sm:w-[90%] lg:w-[80%] mx-auto mt-10 md:mt-20">
                 <div className="flex gap-6">
                     <Link href="/">
-                        <p className="text-3xl md:text-4xl text-white font-bold">X</p>
+                        <p className="text-3xl md:text-4xl 2xl:text-6xl text-white font-bold">X</p>
                     </Link>
-                    <h1 className="text-3xl md:text-4xl text-white font-bold">ABOUT ME</h1>
+                    <h1 className="text-3xl md:text-4xl 2xl:text-6xl text-white font-bold">ABOUT ME</h1>
                 </div>
 
                 {/* content */}
                 <div className="mt-10 md:mt-14 text-white/80 text-sm md:text-base">
-                    <p className="md:w-[75%] leading-loose 2xl:text-lg">Halo, saya Achmad Fauzan Husain. Seorang fullstack web developer yang juga gemar mendesain web. Sedang sibuk mengejar mimpi sambil menikmati prosesnya. Biasa diberi ucapan selamat pada tanggal 13 agustus</p>
+                    <p className="md:w-[75%] leading-loose 2xl:leading-loose 2xl:text-xl">Halo, saya Achmad Fauzan Husain. Seorang fullstack web developer yang juga gemar mendesain web. Sedang sibuk mengejar mimpi sambil menikmati prosesnya. Biasa diberi ucapan selamat pada tanggal 13 agustus</p>
 
                     <div className="mt-8 flex flex-col sm:flex-row gap-5">
                         <Image
@@ -24,9 +24,9 @@ const About = () => {
                             className="flex-1 w-full max-h-[400px] rounded-2xl object-cover object-bottom"
                         />
 
-                        <div className="flex justify-between gap-4 flex-col flex-1 leading-loose 2xl:text-lg">
+                        <div className="flex justify-between gap-4 flex-col flex-1 leading-loose 2xl:leading-loose 2xl:text-xl">
                             <div>
-                                <p className="mt-4">Saya belajar pembuatan website disaat kelas 2 smp secara otodidak dan melanjutkan belajar di smk di jurusan rekayasa perangkat lunak.</p>
+                                <p className="mt-4 2xl:mt-0">Saya belajar pembuatan website disaat kelas 2 smp secara otodidak dan melanjutkan belajar di smk di jurusan rekayasa perangkat lunak.</p>
                                 <p className="mt-8">Sekarang saya mengambil jurusan Rekayasa Perangkat Lunak di <span className="font-semibold">Telkom University</span>.</p>
                                 <p className="mt-8">Saya suka olahraga dan sesekali nonton film.</p>
                             </div>
