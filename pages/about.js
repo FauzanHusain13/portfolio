2b35ec700b6ef5f307c712890288e4dcecfab3b1
Fauzan Husain <achmadfauzan1340@gmail.com>
2025-04-29 18:17:40 +0800

    change language at about me page

--- pages/about.js
+++ pages/about.js
@@ -14,7 +14,9 @@ const About = () => {
 
                 {/* content */}
                 <div className="mt-10 md:mt-14 text-white/80 text-sm md:text-base">
-                    <p className="md:w-[75%] leading-loose 2xl:leading-loose 2xl:text-xl">Halo, saya Achmad Fauzan Husain. Seorang fullstack web developer yang juga gemar mendesain web. Sedang sibuk mengejar mimpi sambil menikmati prosesnya. Biasa diberi ucapan selamat pada tanggal 13 agustus</p>
+                    <p className="md:w-[75%] leading-loose 2xl:leading-loose 2xl:text-xl">
+                        Hi! I’m Achmad Fauzan Husain — a fullstack web developer who also enjoys designing websites. Right now, I’m chasing my dreams while enjoying every step of the journey. People usually wish me a happy birthday on August 13th!
+                    </p>
 
                     <div className="mt-8 flex flex-col sm:flex-row gap-5">
                         <Image
@@ -22,26 +24,27 @@ const About = () => {
                             width={200}
                             height={0}
                             className="flex-1 w-full max-h-[400px] rounded-2xl object-cover object-bottom"
+                            alt="About me image"
                         />
 
                         <div className="flex justify-between gap-4 flex-col flex-1 leading-loose 2xl:leading-loose 2xl:text-xl">
                             <div>
-                                <p className="mt-4 2xl:mt-0">Saya belajar pembuatan website disaat kelas 2 smp secara otodidak dan melanjutkan belajar di smk di jurusan rekayasa perangkat lunak.</p>
-                                <p className="mt-8">Sekarang saya mengambil jurusan Rekayasa Perangkat Lunak di <span className="font-semibold">Telkom University</span>.</p>
-                                <p className="mt-8">Saya suka olahraga dan sesekali nonton film.</p>
+                                <p className="mt-4 2xl:mt-0">I started learning web development by myself back in junior high school (around 8th grade), then went on to study software engineering in vocational high school.</p>
+                                <p className="mt-8">Now, I’m majoring in Software Engineering at <span className="font-semibold">Telkom University</span>.</p>
+                                <p className="mt-8">Outside of coding, I enjoy sports and watching movies every now and then.</p>
                             </div>
 
                             <div className="mb-12">
                                 <p className="text-sm">follow my:</p>
                                 <div className="flex gap-10 mt-5">
                                     <Link href="https://www.instagram.com/_fauzanhusain/">
-                                        <Image src="/icon/instagram.png" width={25} height={0} className="2xl:w-[35px]" />
+                                        <Image src="/icon/instagram.png" width={25} height={0} className="2xl:w-[35px]" alt="Instagram" />
                                     </Link>
                                     <Link href="https://www.linkedin.com/in/fauzan-husain-182851246/">
-                                        <Image src="/icon/linkedin.png" width={25} height={0} className="2xl:w-[35px]" />
+                                        <Image src="/icon/linkedin.png" width={25} height={0} className="2xl:w-[35px]" alt="LinkedIn" />
                                     </Link>
                                     <Link href="https://github.com/FauzanHusain13">
-                                        <Image src="/icon/github.png" width={25} height={0} className="2xl:w-[35px]" />
+                                        <Image src="/icon/github.png" width={25} height={0} className="2xl:w-[35px]" alt="GitHub" />
                                     </Link>
                                 </div>
                             </div>
